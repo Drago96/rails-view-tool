f17524270ddf2a2cab831dfed0c82f8d717f09e0
Drago96 <dragproychev@gmail.com>
2018-12-30 23:05:52 +0200

    Initial commit

---
+++ lib/proychev_view_tool.rb
@@ -0,0 +1,7 @@
+require 'proychev_view_tool/version'
+require 'proychev_view_tool/renderer'
+
+module ProychevViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/proychev_view_tool/version.rb
@@ -0,0 +1,3 @@
+module ProychevViewTool
+  VERSION = '0.1.0'
+end
